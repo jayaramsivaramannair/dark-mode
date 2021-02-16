6157983897e204a317ca58edf77e01850706fc35
jayaramsivaramannair <jayaramsivaramannair@gmail.com>
2021-02-16 18:37:55 -0500

    Used the custom hook called useDarkMode inside the App component

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,7 +1,7 @@
-import { useState } from 'react';
-import useLocalStorage from './useLocalStorage.js';
 
-export function useDarkMode(defaultMode) {
+import useLocalStorage from './useLocalStorage.js'
+
+export default function useDarkMode(defaultMode) {
     //This custom hook merely composes useLocalStorage
     const [theme, setTheme] = useLocalStorage('darkMode', defaultMode);
     return [theme, setTheme];
--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-export function useLocalStorage(key, initialValue) {
+export default function useLocalStorage(key, initialValue) {
     const [storedValue, setStoredValue] = useState(() => {
         if (localStorage.getItem(key)) {
             return JSON.parse(localStorage.getItem(key))
